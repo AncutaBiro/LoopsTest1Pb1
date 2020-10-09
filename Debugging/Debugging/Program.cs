fbf8c2fb94096da720a743f79c9df8dd58d1f6dd
AncutaBiro <ancuta.biro@yahoo.com>
2020-10-09 22:46:19 +0300

    solving problem

---
+++ Debugging/Debugging/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+ static void Main(string[] args)
+ {
+            int[] numbers = { 2, 3, 5, 7, 25, 15, 9, 30 };
+            string[] result = new string[8];
+        
+   for (int i = 0; i < numbers.Length; i++)
+                {
+                   result[i] = "";
+            
+                   if (numbers[i] % 3 == 0)
+                         result[i] += "Fizz";
+            
+                   if (numbers[i] % 5 == 0)
+                         result[i] += "Buzz";
+           
+                   if (result[i] == "")
+                          result[i] += numbers[i].ToString();
+                }
+        
+    for (int i = 0; i < result.Length; i++)
+                    Console.WriteLine(result[i]);
+      
+   Console.Read();
+          }
+    }
---
+++ LoopsTest1Pb2/LoopsTest1Pb2/Program.cs
@@ -0,0 +1,47 @@
+﻿
+/*Să se scrie o aplicație consolă ce determină cel mai mare element dintr-un șir de numere întregi dat.
+Numerele se introduc câte unul pe linie, iar aplicația le va citi pe rând până cand utilizatorul introduce numărul 0. 
+Când s-a introdus numărul 0 aplicația știe că au fost introduse toate numerele din șir și îl afișează pe cel mai mare dintre ele.
+
+Numărul 0 face și el parte din șir și dacă e cel mai mare număr introdus de utilizator atunci aplicația va returna 0.
+
+Exemplu:
+
+Pentru datele de intrare:
+
+24
+1
+-3
+3
+-1
+0
+La consolă se va afișa:
+
+24
+ */
+
+using System;
+
+namespace LoopsTest1Pb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                string inputData = Console.ReadLine();
+                int n = Convert.ToInt32(inputData);
+
+                int max = 0;
+
+                while (n!= 0)
+                {
+                max = max > n ? max : n;    
+                inputData = Console.ReadLine();
+                n = Convert.ToInt32(inputData);
+        }
+                Console.WriteLine(max);
+                Console.Read();
+        }
+        
+    }
+}
