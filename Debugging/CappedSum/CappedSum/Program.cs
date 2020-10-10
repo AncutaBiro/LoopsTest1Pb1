b30a5b3c3415d588eed89d9a9211a58745db0c59
AncutaBiro <ancuta.biro@yahoo.com>
2020-10-10 16:44:41 +0300

    solving test

---
+++ Debugging/CappedSum/CappedSum/Program.cs
@@ -0,0 +1,46 @@
+﻿/*
+ Să se scrie o aplicație consolă ce citește de la tastatură o listă de numere până când suma lor ajunge la o anumită limită dată.
+Pe prima linie se dă limita, iar pe următoarele linii urmează lista de numere ce trebuie adunate. Programul se va opri din citit când suma numerelor 
+introduse până atunci depășește limita și va afișa ultima sumă ce nu depășea limita, precum și de căte numere a fost nevoie pentru a o calcula.
+Sugestie: Folosiți execuția pas cu pas pentru a localiza și fixa eroarea din proiectul atașat.
+
+Exemplu:
+
+Pentru datele de intrare:
+
+10
+2
+3
+2
+6
+La consolă se va afișa:
+
+7 3
+ */
+
+using System;
+
+namespace CappedSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sumLimit = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            int count = 0;
+            int number = 0;
+
+            while (sum < sumLimit)
+            {
+              number = Convert.ToInt32(Console.ReadLine());
+              sum += number;
+              count++;
+            }
+
+            Console.WriteLine((sum - number )+ " " + (count-1));
+            Console.ReadLine();
+        
+        }
+    }
+}
---
+++ Debugging/CountOccurrences/CountOccurrences/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace CountOccurrences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] values = { 1, -1, 1 };
+
+            int sum = 0;
+            int firstIndex = 0;
+
+            while (sum != 0)
+            {
+                sum += values[firstIndex];
+                firstIndex++;
+            }
+
+            int secondIndex = 0;
+            sum = 0;
+
+            do
+            {
+                sum += values[secondIndex];
+                secondIndex++;
+            } while (sum != 0);
+
+            int thirdIndex = 0;
+            sum = 0;
+
+            for (thirdIndex = 0; thirdIndex < values.Length; thirdIndex++)
+            {
+                sum += values[thirdIndex];
+            }
+
+
+
+          
+        }
+    }
+}
