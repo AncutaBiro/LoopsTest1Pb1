5bfd845a3fbb0e7896212e27f7cce688e753b425
AncutaBiro <ancuta.biro@yahoo.com>
2020-10-10 22:57:05 +0300

    testing debugging in small apps

---
+++ Debugging/testQuiz1/testQuiz1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace testQuiz1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = Console.ReadLine();
+            string result = "";
+
+            while (line != "gata")
+            {
+                int number = Convert.ToInt32(line);
+                result += number * 2 + " ";
+                line = Console.ReadLine();
+
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
+
